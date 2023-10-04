5ac2d7e1cc0092ca1f75f36e137176d8c6c6d2b2
Waffle <adolfo.espinosa@sansano.usm.cl>
2023-10-04 01:32:30 -0300

    Create getAllDoctors
    Create a method to get all doctors in the DB

--- MedDirectory/server/controllers/DoctorControllers.js
+++ MedDirectory/server/controllers/DoctorControllers.js
@@ -1,15 +1,17 @@
-import Doctors from '../models/doctorModel';
-import bcrypt from 'bcryptjs';
+import Doctors from '../model/doctorModel.js';
+import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 // Register a new doctor and return a token
 const doctorRegister = async (req, res, next) => {
     try {
         const { name, email, password, specialization, experience} = req.body;
+        console.log("inicio")
         const emailCheck = await Doctors.findOne({ email });
         if (emailCheck) {
             return res.json({ message: 'Email already exists', status: false });
         }
+        console.log("emailCheck")
         const hashedPassword = await bcrypt.hash(password, 10);
         const doctor = await Doctors.create({
             name,
@@ -18,6 +20,7 @@ const doctorRegister = async (req, res, next) => {
             specialization,
             experience
         });
+        console.log("doctor created")
         const token = jwt.sign({ id: doctor._id }, process.env.JWT_SECRET, { expiresIn: '30d' });
         res.status(201).json({ token });
     } catch (error) {
@@ -44,4 +47,15 @@ const doctorLogin = async (req, res, next) => {
     };
 }
 
-export { doctorRegister, doctorLogin };
+const getAllDoctors = async (req, res, next) => {
+    try {
+        const doctors = await Doctors.find().select([
+            "name", "specialization", "experience", "avatarImage",
+        ]);
+        return res.status(200).json(doctors);
+    } catch (error) {
+        next(error);
+    };
+}
+
+export { doctorRegister, doctorLogin, getAllDoctors };
--- MedDirectory/server/routes/doctorRoute.js
+++ MedDirectory/server/routes/doctorRoute.js
@@ -5,11 +5,12 @@
 //     doctorLogin
 // } = require('../controllers/DoctorControllers'); // Usar la misma capitalización que el nombre del archivo
 
-import { doctorRegister, doctorLogin } from "../controllers/DoctorControllers.js";
+import { doctorRegister, doctorLogin, getAllDoctors } from "../controllers/DoctorControllers.js";
 import { Router } from "express";
 const router = Router();
 
 router.post("/doctorRegister", doctorRegister);
 router.post("/doctorLogin", doctorLogin);
+router.get("/getAllDoctors", getAllDoctors);
 
 export default router;
