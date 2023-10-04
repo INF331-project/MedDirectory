e24d5a4f0d50e25e45dcb8eb38b3a34d19a95744
Waffle <adolfo.espinosa@sansano.usm.cl>
2023-10-04 02:30:42 -0300

    Create getDoctorbyName and UpdateDoctorbyId
    Create getDoctorbyName and UpdateDoctorbyId.
    The first one must be passed the name of the doctor as a parameter.
    The second one must be passed the id of the doctor as a parameter and the updated field as a body.

--- MedDirectory/server/routes/doctorRoute.js
+++ MedDirectory/server/routes/doctorRoute.js
@@ -5,12 +5,14 @@
 //     doctorLogin
 // } = require('../controllers/DoctorControllers'); // Usar la misma capitalización que el nombre del archivo
 
-import { doctorRegister, doctorLogin, getAllDoctors } from "../controllers/DoctorControllers.js";
+import { doctorRegister, doctorLogin, getAllDoctors, getDoctorbyName, UpdateDoctorbyId } from "../controllers/DoctorControllers.js";
 import { Router } from "express";
 const router = Router();
 
 router.post("/doctorRegister", doctorRegister);
 router.post("/doctorLogin", doctorLogin);
 router.get("/getAllDoctors", getAllDoctors);
+router.get("/getDoctorbyName/:name", getDoctorbyName);
+router.put("/UpdateDoctorbyId/:id", UpdateDoctorbyId);
 
 export default router;
--- MedDirectory/test/server.test.js
+++ MedDirectory/test/server.test.js
@@ -1,7 +1,23 @@
-const app = require('../server/app');
-const request = require('supertest');
+import app from '../server/app.js';
+import request from 'supertest';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+beforeAll(async () => {
+    await mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+});
+
+afterAll(async () => {
+    await mongoose.connection.close();
+});
 
 describe('API test', () => {
+
     test('It should response with 200 status code from GET /ping', async () => {
         const response = await request(app).get('/ping').send();
         expect(response.statusCode).toBe(200);
@@ -12,18 +28,47 @@ describe('API test', () => {
         expect(response.statusCode).toBe(200);
     });
     
-    test('It should response with 201 status code from /doctorAPI/doctorRegister', async () => {
-        const response = await request(app)
-            .post('/doctorAPI/doctorRegister/')
-            .send({
-                name: 'John Doe',
-                email: 'johndoe@example.com',
-                password: 'password123',
-                specialization: 'Cardiologist',
-                experience: '5 years'
-            });
+    // test('It should response with 201 status code from /doctorAPI/doctorRegister', async () => {
+    //     const response = await request(app)
+    //         .post('/doctorAPI/doctorRegister/')
+    //         .send({
+    //             name: 'John Doe',
+    //             email: 'johndoe@example.com',
+    //             password: 'password123',
+    //             specialization: 'Cardiologist',
+    //             experience: '5 years'
+    //         });
 
-        // Verifica que la respuesta tenga un código de estado 201
-        expect(response.status).toBe(201);
+    //     // Verifica que la respuesta tenga un código de estado 201
+    //     expect(response.status).toBe(201);
+    // });
+
+    test('It should response with 200 status code from /doctorAPI/getDoctorbyName', async () => {
+        const name = "John Doe"
+        const response = await request(app)
+            .get('/doctorAPI/getDoctorbyName/' + name)
+            .send();
+
+        // Verifica que la respuesta tenga un código de estado 200
+        expect(response.status).toBe(200);
+    });
+
+    test("It should response with 200 status code from /doctorAPI/getAllDoctors", async () => {
+        const response = await request(app)
+            .get("/doctorAPI/getAllDoctors")
+            .send();
+
+        expect(response.status).toBe(200);
+    });
+
+    test("It should response with 200 status code from /doctorAPI/UpdateDoctorbyId", async () => {
+        const response = await request(app)
+            .put("/doctorAPI/UpdateDoctorbyId/651ceefa78da9f80b0651e5d")
+            .send({
+                name: "John Doe",
+                email: "jhon.doe2@a.com",
+                specialization: "Traumatologist",
+            });
+        expect(response.status).toBe(200);
     });
 })
