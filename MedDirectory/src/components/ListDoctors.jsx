9c44275ced4b263f4af520a821916890e68fa99b
Pipe <felipe.rojass@sansano.usm.cl>
2023-10-05 01:22:06 -0300

    front_SearchMed #comment Finished
    Finaliza la implementación de barra de búsqueda por nombre para médicos

--- MedDirectory/src/components/ListDoctors.jsx
+++ MedDirectory/src/components/ListDoctors.jsx
@@ -5,13 +5,16 @@ import { useNavigate } from "react-router-dom";
 
 function DoctorList() {
   const [doctors, setDoctors] = useState([]);
+  const [name, setName] = useState('');
+  const [foundMed, setFoundMed] = useState(doctors);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(getAllDoctorsRoute);
         setDoctors(response.data);
-        console.log(response.data);
+        setFoundMed(doctors);
+        //console.log(response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -20,23 +23,49 @@ function DoctorList() {
     fetchData();
   }, []);
 
-    const navigate = useNavigate();
-    const sendID = (doctor) =>{
-      navigate('/editmed', {state : {doctor: doctor}})
+  const navigate = useNavigate();
+  const sendID = (doctor) =>{
+    navigate('/editmed', {state : {doctor: doctor}})
+  }
+
+
+  const filter = (e) => {
+    const keyword = e.target.value;
+
+    if (keyword !== '') {
+      const results =  doctors.filter((doctor) => {
+        return doctor.name.toLowerCase().startsWith(keyword.toLowerCase());
+      });
+      setFoundMed(results);
+    } else {
+      setFoundMed(doctors);
     }
+    setName(keyword);
+  };
 
   return (
     <div>
       <h2>List of Doctors</h2>
-      <ul>
-        {doctors.map((doctor) => (
+      <input 
+        type="search"
+        value={name}
+        onChange={filter}
+        className="input"
+        placeholder="nombre"
+      />
+      {foundMed && foundMed.length > 0 ? (
+        foundMed.map((doctor) => (
           <li key={doctor._id}>
             {doctor.name}, {doctor.specialization}, {doctor.experience} of
             experience.
             <button onClick={() => {sendID(doctor._id)}}>Editar</button>
           </li>
-        ))}
-      </ul>
+        ))
+      ) : (
+        <div>
+          <a>No se encuentran resultados</a>
+        </div>
+      )}
     </div>
   );
 }
