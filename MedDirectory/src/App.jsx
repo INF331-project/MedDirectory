3fd6ec1de79f7ba0cfbff6df6623c91a301dad5f
Waffle <adolfo.espinosa@sansano.usm.cl>
2023-10-30 15:43:03 -0300

    MEDD-9 h2

--- MedDirectory/src/App.jsx
+++ MedDirectory/src/App.jsx
@@ -16,7 +16,7 @@ export default function App() {
     <>
       <BrowserRouter>
       <NavBar />
-      <h1>Directorio Médico</h1>
+      <h1>Directorio Médic</h1>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/editmed" element={<EditMed />} />
