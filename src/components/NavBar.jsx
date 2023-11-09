c743324b87b8737e14b53b5b6c391a4ba409ea17
Waffle <adolfo.espinosa@sansano.usm.cl>
2023-11-08 22:55:42 -0300

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useLoadScript } from '@react-google-maps/api';
-import { Dropdown } from "react-bootstrap";
-import usePlacesAutocomplete from "use-places-autocomplete";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
