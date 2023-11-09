e915f5bd175a80df4643e82ded15c51c405bfe65
Waffle <adolfo.espinosa@sansano.usm.cl>
2023-11-08 23:03:04 -0300

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,6 +15,7 @@ export const NavBar = () => {
     });
 
     const renderClinics = () => {
+        console.log("api: ", import.meta.env.VITE_GOOGLE_MAPS_API_KEY);
         if (!isLoaded) return <div>'Loading...'</div>;
         return (
             <Clinics />
