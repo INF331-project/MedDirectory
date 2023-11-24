c480c011a3e4b46dca93249d24e0da297dec55f3
Waffle <adolfo.espinosa@sansano.usm.cl>
2023-11-23 23:03:13 -0300

    selenium test

---
+++ src/__test__/Home_Test.js
@@ -0,0 +1,21 @@
+import 'chromedriver';
+import { equal } from 'assert';
+import { Builder, Key, By, until } from 'selenium-webdriver';
+
+describe('Checkout Google.com', function () {
+    let driver;
+
+    before(async function() {
+        driver = await new Builder().forBrowser('chrome').build();
+    });
+
+    it('Search on Google', async function() {
+        await driver.get('https://meddirectory.azurewebsites.net/');
+        await driver.findElement(By.linkText("Lista de médicos")).click();
+
+        let title = await driver.findElement(By.className("input form-control")).getAttribute("placeholder");
+        equal(title, "Nombre del médico");
+    });
+
+    after(() => driver && driver.quit());
+})
