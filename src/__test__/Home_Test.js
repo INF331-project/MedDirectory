08d9b257aeab232f2a596971a0a3e5ac13324963
Waffle <adolfo.espinosa@sansano.usm.cl>
2023-11-23 23:40:49 -0300

    c

--- src/__test__/Home_Test.js
+++ src/__test__/Home_Test.js
@@ -1,15 +1,18 @@
 import 'chromedriver';
 import { equal } from 'assert';
 import { Builder, Key, By, until } from 'selenium-webdriver';
+import chrome from 'selenium-webdriver/chrome.js';
+const screen = {
+    width: 640,
+    height: 480
+  };
 
 describe('Checkout Google.com', function () {
     let driver;
 
     before(async function() {
 
-        headless = process.env.HEADLESS? ['headless'] : []
-
-        driver = await new Builder().forBrowser('chrome').build();
+        driver = await new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().headless().windowSize(screen)).build();
 
     });
 
