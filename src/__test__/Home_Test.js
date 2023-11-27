c63372a003a470ea289587eb55dc4939ef60f01e
Pipe <felipe.rojass@sansano.usm.cl>
2023-11-27 17:02:20 -0300

    New Sel Test Add doctor
    Test automatizado en selenium para añadir doctor

--- src/__test__/Home_Test.js
+++ src/__test__/Home_Test.js
@@ -2,9 +2,10 @@ import 'chromedriver';
 import { equal } from 'assert';
 import { Builder, Key, By, until } from 'selenium-webdriver';
 import chrome from 'selenium-webdriver/chrome.js';
+
 const screen = {
-    width: 640,
-    height: 480
+    width: 1920,
+    height: 1080
   };
 
 describe('Checkout Google.com', function () {
@@ -23,6 +24,22 @@ describe('Checkout Google.com', function () {
         let title = await driver.findElement(By.className("input form-control")).getAttribute("placeholder");
         equal(title, "Nombre del médico");
     });
+    
+    it('Crear médico', async function() {
+        await driver.get('https://meddirectory.azurewebsites.net/');
+        await driver.findElement(By.linkText("Añadir médico")).click();
 
+        await driver.findElement(By.id("name")).sendKeys("Prueba Selenium");
+        await driver.findElement(By.id("email")).sendKeys("prueba@selenium.com");
+        await driver.findElement(By.id("password")).sendKeys("prueba");
+        await driver.findElement(By.id("specialization")).sendKeys("prueba selenium");
+        await driver.findElement(By.id("experience")).sendKeys("prueba selenium");
+
+        await driver.findElement(By.className("btn btn-primary")).click();
+
+        await driver.wait(until.elementLocated(By.className("input form-control")), 10000);
+
+    });
+    
     after(() => driver && driver.quit());
 })
